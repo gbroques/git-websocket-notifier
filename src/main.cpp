8b789808c33ca8b9a8f3bf3dccdd4f3b4b368d7c
G Roques <groques360@gmail.com>
2025-01-07 04:30:51 -0600

    Emit edges for tree

--- src/main.cpp
+++ src/main.cpp
@@ -61,21 +61,49 @@ namespace graph {
   }
 }
 
-graph::node get_node(git_odb* odb, const git_oid* oid) {
+graph::node get_node(git_repository* repo, git_odb* odb, const git_oid* oid) {
   git_odb_object* object = nullptr;
   int error = git_odb_read(&object, odb, oid);
   std::string id = git_oid_tostr_s(oid);
   if (error < 0) {
     std::cerr << "Error reading object with OID " << id << std::endl;
+    git_odb_free(odb);
+    git_repository_free(repo);
     exit(1);
   }
   git_object_t object_type = git_odb_object_type(object);
   const char* type = git_object_type2string(object_type);
   size_t size = git_odb_object_size(object);
-  const char* content = (const char*) git_odb_object_data(object);
+
+  const char* content = (char*) git_odb_object_data(object);
   std::string string_content(content);
+  // Lookup tree for content
+  if (type == "tree") {
+    git_tree* tree = nullptr;
+    if (git_tree_lookup(&tree, repo, oid) < 0) {
+      std::cerr << "Failed to lookup tree: " << git_error_last()->message << std::endl;
+      git_odb_free(odb);
+      git_repository_free(repo);
+      exit(1);
+    }
+    const git_tree_entry *entry;
+    size_t i, count;
+    string_content = "";
+    for (i = 0, count = git_tree_entrycount(tree); i < count; i++) {
+      entry = git_tree_entry_byindex(tree, i);
+      char buffer[size];
+      sprintf(buffer, "%06o %s %s\t%s\n",
+                 git_tree_entry_filemode_raw(entry),
+                 git_object_type2string(git_tree_entry_type(entry)),
+                 git_oid_tostr_s(git_tree_entry_id(entry)),
+                 git_tree_entry_name(entry));
+      std::string buffer_string = buffer;
+      string_content += buffer_string;
+    }
+  }
+
   std::cout << id.substr(0, 10) << " " << std::setw(6) << type << " " << size << std::endl;
-  graph::node node({id, type, size, content});
+  graph::node node({id, type, size, string_content});
   git_odb_object_free(object);
   return node;
 }
@@ -90,19 +118,36 @@ std::vector<graph::edge> get_edges(graph::node node) {
     graph::edge edge({node.id + treeId, node.id, treeId});
     edges.push_back(edge);
   }
+  if (node.type == "tree") {
+    // Tree contents are the following:
+    // 100644 blob a906cb2a4a904a152e80877d4088654daad0c859      README
+    // 100644 blob 8f94139338f9404f26296befa88755fc2598c289      Rakefile
+    // 040000 tree 99f1a6d12cb4b6f19c8655fca46c3ecf317074e0      lib
+    std::stringstream stream(node.content);
+    std::string line;
+
+    while (std::getline(stream, line)) {
+      std::cout << line << std::endl;
+      std::string target = line.substr(12, 40);
+      graph::edge edge({node.id + target, node.id, target});
+      edges.push_back(edge);
+    }
+  }
   return edges;
 }
 
 struct args {
+  git_repository* repo;
   git_odb* odb;
   std::shared_ptr<WsClient::Connection> connection;
 };
 
 int send_object(const git_oid* oid, void* payload) {
   args* args_pointer = (args*) payload;
+  git_repository* repo = args_pointer->repo;
   git_odb* odb = args_pointer->odb;
   std::shared_ptr<WsClient::Connection> connection = args_pointer->connection;
-  graph::node node = get_node(odb, oid);
+  graph::node node = get_node(repo, odb, oid);
   connection->send(boost::json::serialize(boost::json::value_from(node)));
   std::vector<graph::edge> edges = get_edges(node);
   for (graph::edge edge : edges) {
@@ -127,7 +172,7 @@ class UpdateListener : public efsw::FileWatchListener {
       connection = _connection;
 
       // Read all existing objects in the object database
-      args payload = {odb, connection};
+      args payload = {repo, odb, connection};
       if (git_odb_foreach(odb, send_object, &payload) != 0) {
         std::cerr << "Failed to iterate over object database: " << git_error_last()->message << std::endl;
         git_odb_free(odb);
@@ -159,7 +204,7 @@ class UpdateListener : public efsw::FileWatchListener {
           std::cerr << "Failed to convert " << sha1 << " to git_oid!" << std::endl;
           return;
         }
-        graph::node node = get_node(odb, &oid);
+        graph::node node = get_node(repo, odb, &oid);
         connection->send(boost::json::serialize(boost::json::value_from(node)));
       }
     }
